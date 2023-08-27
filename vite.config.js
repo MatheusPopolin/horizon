dade79b9d66bb23b4ea54625db1c3f97eb4a7865
Matheus Popolin <matheuspopolin@hotmail.com>
2023-08-27 11:55:51 -0300

    deploy2

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue': 'vue/dist/vue.esm-bundler.js',
-    },
-    build: {
-      outDir: './', // A raiz absoluta do projeto
     },
   }
 })
